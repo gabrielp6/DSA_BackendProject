4e79872938375a8caa49caed1dc43ad57b24d79f
gabrielp6 <gabrielpastors6@gmail.com>
2021-06-18 16:35:42 +0200

    funciona toda la web

--- public/javascripts/partidasusuario.js
+++ public/javascripts/partidasusuario.js
@@ -1,62 +1,25 @@
 var BASE_URI = "http://localhost:8080/dsaApp"
 
-$(document).ready(function() {
+$(document).ready(function(e) {
 
-    $('#btnVerPartidas').click(function () {
+    $('#btnVerPartidas').click(function (e) {
         var usuario = $('#username').val();
+        e.preventDefault();
+
         $.ajax({
             url: BASE_URI.concat("/estadisticas/partidasUsuario/"+usuario),
                 success: function(data) {
                     alert("recibido");
-                    $("#enemigosMatados1").text(data[0].enemigosMatados);
-                    $("#tiempo1").text(data[0].tiempo);
-                    $("#monedasRecogidas1").text(data[0].monedasRecogidas);
-                    $("#puntuacionFinal1").text(data[0].puntuacionFinal);
 
-                    $("#enemigosMatados2").text(data[1].enemigosMatados);
-                    $("#tiempo2").text(data[1].tiempo);
-                    $("#monedasRecogidas2").text(data[1].monedasRecogidas);
-                    $("#puntuacionFinal2").text(data[1].puntuacionFinal);
+                    var inHTML = "<tr><td align='center' style='font-size:150%'><strong>Enemigos matados</strong></td><td align='center' style='font-size:150%'><strong>Tiempo</strong></td><td align='center' style='font-size:150%''><strong>Monedas recogidas</strong></td><td align='center'' style='font-size:150%'><strong>Puntuación final</strong></td></tr>";
+                    alert("r");
+                    var extra = "";
+                    $.each(data, function(index, value){
+                        var newItem = "<tr><td align='center'' style='font-size:125%'>"+ value.enemigosMatados + "</td><td align='center' style='font-size:125%'>"+ value.tiempo +"</td><td align='center' style='font-size:125%'>"+ value.monedasRecogidas +"</td><td align='center' style='font-size:125%'>"+ value.puntuacionFinal +"</td></tr>"
+                        extra += newItem;
+                    });
 
-                    $("#enemigosMatados3").text(data[2].enemigosMatados);
-                    $("#tiempo3").text(data[2].tiempo);
-                    $("#monedasRecogidas3").text(data[2].monedasRecogidas);
-                    $("#puntuacionFinal3").text(data[2].puntuacionFinal);
-
-                    $("#enemigosMatados4").text(data[3].enemigosMatados);
-                    $("#tiempo4").text(data[3].tiempo);
-                    $("#monedasRecogidas4").text(data[3].monedasRecogidas);
-                    $("#puntuacionFinal4").text(data[3].puntuacionFinal);
-
-                    $("#enemigosMatados5").text(data[4].enemigosMatados);
-                    $("#tiempo5").text(data[4].tiempo);
-                    $("#monedasRecogidas5").text(data[4].monedasRecogidas);
-                    $("#puntuacionFinal5").text(data[4].puntuacionFinal);
-
-                    $("#enemigosMatados6").text(data[5].enemigosMatados);
-                    $("#tiempo6").text(data[5].tiempo);
-                    $("#monedasRecogidas6").text(data[5].monedasRecogidas);
-                    $("#puntuacionFinal6").text(data[5].puntuacionFinal);
-
-                    $("#enemigosMatados7").text(data[6].enemigosMatados);
-                    $("#tiempo7").text(data[6].tiempo);
-                    $("#monedasRecogidas7").text(data[6].monedasRecogidas);
-                    $("#puntuacionFinal7").text(data[6].puntuacionFinal);
-
-                    $("#enemigosMatados8").text(data[7].enemigosMatados);
-                    $("#tiempo8").text(data[7].tiempo);
-                    $("#monedasRecogidas8").text(data[7].monedasRecogidas);
-                    $("#puntuacionFinal8").text(data[7].puntuacionFinal);
-
-                    $("#enemigosMatados9").text(data[8].enemigosMatados);
-                    $("#tiempo9").text(data[8].tiempo);
-                    $("#monedasRecogidas9").text(data[8].monedasRecogidas);
-                    $("#puntuacionFinal9").text(data[8].puntuacionFinal);
-
-                    $("#enemigosMatados10").text(data[9].enemigosMatados);
-                    $("#tiempo10").text(data[9].tiempo);
-                    $("#monedasRecogidas10").text(data[9].monedasRecogidas);
-                    $("#puntuacionFinal10").text(data[9].puntuacionFinal);
+                    $("table#dinamica").html(inHTML + extra);
                 }
 
         })
